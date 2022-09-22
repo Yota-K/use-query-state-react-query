df5a107e6d200fc27175f34c8078c37e3a2a0fb7
Y.K <powdersugar828828@gmail.com>
2022-09-23 01:20:05 +0900

    test: Added hooks test.

--- src/test/useQueryState.test.ts
+++ src/test/useQueryState.test.ts
@@ -1,18 +1,37 @@
 import { describe, test, expect } from 'vitest';
-import { renderHook } from '@testing-library/react';
+import { renderHook, act, waitFor } from '@testing-library/react';
 import { useQueryState } from '../core/index';
 import { createMockProvider } from '../mock/mockProvider';
 
 describe('useQueryState', () => {
-  test('Set global state.', () => {
+  const initialSettings = () => {
     const { MockProvider } = createMockProvider();
-    const { result } = renderHook(() => useQueryState(['TEST'], { id: 1, userName: 'Hoge', isLoggedIn: true }), {
+    const { result } = renderHook(() => useQueryState(['TEST'], { userName: 'Hoge', isLoggedIn: true }), {
       wrapper: MockProvider,
     });
+    return result;
+  };
 
+  test('Set global state.', () => {
+    const result = initialSettings();
     const [getter] = result.current;
 
+    expect(getter.userName).toBe('Hoge');
     expect(getter.isLoggedIn).toBeTruthy();
   });
-  // test('Read global state.', () => {});
+
+  test('Change global state.', async () => {
+    const result = initialSettings();
+
+    // change state value
+    await act(async () => {
+      const [getter, setter] = result.current;
+      setter({ ...getter, userName: '', isLoggedIn: false });
+    });
+
+    // Using React 18 or later, Required "waitFor"
+    // https://tanstack.com/query/v4/docs/guides/testing#our-first-test
+    await waitFor(() => expect(result.current[0].userName).toBe(''));
+    await waitFor(() => expect(result.current[0].isLoggedIn).toBe(false));
+  });
 });
