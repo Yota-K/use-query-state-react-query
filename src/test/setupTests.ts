286382dbfb94ad3e72102af96e383256c98f075f
Y.K <powdersugar828828@gmail.com>
2022-09-25 03:07:32 +0900

    feat: Setup @testing-library/jest-dom.

---
+++ src/test/setupTests.ts
@@ -0,0 +1,3 @@
+// In order to use "toBeInTheDocument" function need 'import @testing-library/jest-dom'.
+// https://stackoverflow.com/questions/56547215/react-testing-library-why-is-tobeinthedocument-not-a-function
+import '@testing-library/jest-dom';
