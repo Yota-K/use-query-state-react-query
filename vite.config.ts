47f28399cf19ff5c731c73a311e288f148d8b0fe
Y.K <powdersugar828828@gmail.com>
2022-09-26 23:05:01 +0900

    feat: export source map.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    sourcemap: true,
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'useQueryStateForReactQuery',
